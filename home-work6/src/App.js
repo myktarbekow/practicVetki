0c14c16d71bfd1a7fffa20fa9b369b38820bb5fc
Ramazan <ramazanmyktarbekuulu@gmail.com>
2022-10-31 15:49:34 +0600

    origin

--- home-work6/src/App.js
+++ home-work6/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -9,17 +9,53 @@ function App() {
   };
 
   const clickMinus = () => {
+    if(count.trim())
     setCount(count - 1);
   };
   const clickDelete = () => {
     setCount((count = 0));
   };
+  // const [value, setValue] = useState("");
+  // const [inputHandler, SetInputHandler] = useState([]);
 
+  // const changeHandler = (e) => {
+  //   setValue(e.target.value);
+  // };
+
+  // const savingValue = () => {
+  //   SetInputHandler([
+  //     ...inputHandler,
+  //     {
+  //       title: value,
+  //       id: Math.random(),
+  //     },
+  //   ]);
+  //   setValue("");
+  // };
+  // const deleteHandler = (id) => {
+  //   const filtered = inputHandler.filter((item) => item.id !== id);
+  //   SetInputHandler(filtered);
+  // };
+
+  // const prev =(e)=>{
+  //   e.preventDefault()
+  // }
   return (
     <div className="App">
+      {/* <form onClick={prev} action="">
+        <input value={value} onChange={changeHandler} type="text" />
+        <button onClick={savingValue}>Save</button>
+        {inputHandler.map((item) => (
+          <li>
+            {item.title}{" "}
+            <button onClick={(id) => deleteHandler(item.id)}>Delete</button>
+          </li>
+        ))}
+      </form> */}
+
       <h2>Counter:</h2>
       <h1>{count}</h1>
-      <button className="minus" onClick={clickMinus} disabled={count === 0}>
+      <button className="minus" onClick={clickMinus}>
         Minus-
       </button>
       <button className="plus" onClick={clickPlus}>
